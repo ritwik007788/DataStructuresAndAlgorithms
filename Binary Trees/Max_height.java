a866cd43e89993f018d2960ff4345727e50591e4
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-02 06:32:28 +0530

    Create Max_height.java

---
+++ Binary Trees/Max_height.java
@@ -0,0 +1,28 @@
+//gfg
+
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        // code here 
+        if(node == null) return 0;
+        
+        int left = height(node.left);
+        int right = height(node.right);
+        return Math.max(left,right)+1;
+    }
+}
