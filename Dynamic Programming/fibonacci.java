5a99fe7acc5b04b666141cbdee921226050638a4
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-12 13:36:34 +0530

    Create fibonacci.java

---
+++ Dynamic Programming/fibonacci.java
@@ -0,0 +1,17 @@
+//leetcode 509
+
+class Solution {
+    public int fib(int n) {
+        int dp[] = new int[31];
+        //initialization
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i = 2 ; i < n+1 ; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
+    }
+}
