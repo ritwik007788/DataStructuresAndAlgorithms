a53f3c0584f79fa9734c5f399f2c32210ee68f8c
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-13 06:20:35 +0530

    Update coinChange_min_no_of_coins.java

--- Dynamic Programming/unboundedKnapsack/coinChange_min_no_of_coins.java
+++ Dynamic Programming/unboundedKnapsack/coinChange_min_no_of_coins.java
@@ -20,13 +20,13 @@ class Solution{
 	    for(int i = 1 ; i <= M ; i++){
 	        for(int j = 1 ; j <=V ; j++){
 	            if(coins[i-1]<=j){
-	                dp[i][j] = Math.min(dp[i][j-coins[i-1]] + 1 , dp[i-1][j]);
+	                dp[i][j] = Math.min(dp[i][j-coins[i-1]] + 1 , dp[i-1][j]); // add 1 as we count the minimum no. of coins 
 	            }
 	            else{
 	                dp[i][j] = dp[i-1][j];
 	            }
 	        }
 	    }
-	    return dp[M][V]!=Integer.MAX_VALUE-1?dp[M][V]:-1;
+	    return dp[M][V]!=Integer.MAX_VALUE-1?dp[M][V]:-1; // return -1 if we get an answer from the 1st row
 	} 
 }
