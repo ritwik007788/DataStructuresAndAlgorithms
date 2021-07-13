3cd2243f1ae6d1015b1faeb3d5b74a2c89f84eed
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-13 09:24:55 +0530

    Update MergeIntervals.java

--- Sorting/MergeIntervals.java
+++ Sorting/MergeIntervals.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals , (a,b) -> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals , (a,b) -> a[0]<b[0]?-1:a[0]>b[0]?1:0);
         
         LinkedList<int[]> merged = new LinkedList<>();
         
