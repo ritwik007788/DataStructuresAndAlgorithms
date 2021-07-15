ac547396be157d98717c4c056319226d61fa2f43
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-15 06:21:02 +0530

    Update minOperations.java

--- Dynamic Programming/Misc/minOperations.java
+++ Dynamic Programming/Misc/minOperations.java
@@ -6,11 +6,9 @@ class Solution
     {
         //code here.
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        if(n==0) return 0;
+       
         dp[0] = 0;
-        dp[1] = 1;
-        
+ 
         for(int i = 2 ; i< n+1 ; i++){
             if(i%2==0){
                 dp[i] = Math.min(1+dp[i-1],1+dp[i/2]);
