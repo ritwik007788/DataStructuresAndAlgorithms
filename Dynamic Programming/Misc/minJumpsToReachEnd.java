d4aef08b9d749e0b6edd3a928976dab2c88a4673
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-19 17:23:22 +0530

    Update minJumpsToReachEnd.java

--- Dynamic Programming/Misc/minJumpsToReachEnd.java
+++ Dynamic Programming/Misc/minJumpsToReachEnd.java
@@ -1,29 +1,44 @@
-//gfg
+//Leetcode 45
+//Commented code is dp approach(O(N^2))
 
-class Solution{
-    static int minJumps(int[] arr){
-        // your code here
+class Solution {
+    public int jump(int[] arr) {
         int n = arr.length;
-        Integer dp[] = new Integer[n+1];
-        dp[n] = 0;
-        for(int i = n-1 ; i>=0 ; i--){
-            if(arr[i]>0){
-                int min = Integer.MAX_VALUE;
-               for(int j = 1 ; j <= arr[i] && i+j < dp.length ; j++){
-                    if(dp[i+j]!=null){
-                        min = Math.min(min,dp[i+j]);
-                    }                
-                }
-                
-                if(min != Integer.MAX_VALUE){
-                    dp[i] = min+1 ;
-                }
-            }
+        if(arr[0] == 0 || n==0 || n==1) return 0;
+        
+//         int[] dp = new int[n];
+//         // Arrays.fill(dp,Integer.MAX_VALUE);
+//         dp[0] = 0;
+        
+//         for(int i = 1 ; i < n ; i++){
+//             dp[i] = Integer.MAX_VALUE;
+//             for(int j = 0 ; j < i ; j++){
+//                 if(j+arr[j]>=i && dp[j]!=Integer.MAX_VALUE){
+//                     dp[i] = Math.min(dp[i],1+dp[j]);
+//                     break;
+//                 }
+//             }
+//         }
+//         if(dp[n-1]==Integer.MAX_VALUE) return -1;
+//         return dp[n-1];
+        
+        int currEnd = 0;
+        int currFarthest = 0;
+        int jumps  = 0;
+        
+        for(int i = 0 ; i < arr.length ; i++){
             
+            currFarthest  = Math.max(currFarthest , i+arr[i]);
+            
+            if(i==currEnd){
+                jumps++;
+                currEnd = currFarthest;
+                if (currEnd >= arr.length - 1) {
+					break;
+				}
+            }
         }
         
-        return dp[0]==null?-1:dp[0];
-        
-        
+        return jumps;
     }
 }
