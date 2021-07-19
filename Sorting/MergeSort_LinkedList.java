3a374278eae82ad9046b8f04b876746efa2f091b
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-19 06:49:31 +0530

    Update MergeSort_LinkedList.java

--- Sorting/MergeSort_LinkedList.java
+++ Sorting/MergeSort_LinkedList.java
@@ -1,17 +1,28 @@
 //Leetcode 148
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head==null || head.next==null)
             return head;
         
-        ListNode mid = findMid(head);
+        ListNode mid = findMid(head);// finding mid
         ListNode newHead = mid.next;
         mid.next = null;
         
-        return merge(sortList(head),sortList(newHead));
+        return merge(sortList(head),sortList(newHead));// calling merge on two sorted lists
     }
     
+    //Merge two sorted lists generic code
     public ListNode merge(ListNode A, ListNode B){
         if(A==null) return B;
         if(B==null) return A;
@@ -44,7 +55,7 @@ class Solution {
 
     }
     
-    
+    //Function to find mid
     public ListNode findMid(ListNode head){
         ListNode f = head;
         ListNode s = head;
