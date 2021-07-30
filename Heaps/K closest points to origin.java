aa5762900d2d808eebaf8bdf5b1ddc7117ca1dc1
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-07-30 14:52:17 +0530

    Create K closest points to origin.java

---
+++ Heaps/K closest points to origin.java
@@ -0,0 +1,40 @@
+//Leetcode 973
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<Pair>(Collections.reverseOrder());
+        
+        for(int[] i : points){
+            maxHeap.add(new Pair(i[0]*i[0]+i[1]*i[1],i[0],i[1]));
+            
+            while(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+        // int i = points.length - k ;
+        int i = 0;
+        int[][] res = new int[k][2];
+        while(maxHeap.size()>0){
+            res[i][0] = maxHeap.peek().x;
+            res[i++][1] = maxHeap.peek().y;
+            maxHeap.poll();
+        }
+        
+        return res;
+    }
+}
+
+class Pair implements Comparable<Pair>{
+    int dist;
+    int x;
+    int y;
+    Pair(int dist , int x , int y){
+        this.dist = dist;
+        this.x = x;
+        this.y = y;
+    }
+    public int compareTo(Pair o)
+    {
+        return this.dist - o.dist; 
+    }
+}
