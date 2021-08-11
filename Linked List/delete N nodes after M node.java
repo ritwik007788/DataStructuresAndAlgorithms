f4b21fe1e433a8b42593c4bff92aff05fb1cf07c
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-08-11 18:08:39 +0530

    Update delete N nodes after M node.java

--- Linked List/delete N nodes after M node.java
+++ Linked List/delete N nodes after M node.java
@@ -1,17 +1,16 @@
+//gfg
 class Solution
 {
     static void linkdelete(Node head, int M, int N)
     {
         // your code here
-        Node dummy = new Node(-1);
-        dummy.next = head;
         
-        Node curr = dummy.next;
+        Node curr =head;
         int count= 0;
         while(curr!=null){
             count++;
             
-            if(count%M == 0){
+            if(count%M == 0){// we have traversed M nodes
                 int temp = N;
                 while(curr.next!=null && temp!=0){
                     Node next = curr.next.next;
