0d4a238ade59176129e1fd3e03410c8eeb483c29
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-08-20 06:38:36 +0530

    Update completeBinaryTree.java

--- Binary Trees/completeBinaryTree.java
+++ Binary Trees/completeBinaryTree.java
@@ -1,5 +1,7 @@
 //gfg
 
+//A complete binary tree is a binary tree in which all the levels are completely filled except possibly the lowest one, which is filled from the left.
+
 class GfG
 {
 	boolean isCompleteBT(Node root)
