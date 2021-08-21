7de661a34569830e9e369790a3bff23a4113f993
Ritwik Mahalik <62108938+ritwik007788@users.noreply.github.com>
2021-08-21 09:04:30 +0530

    Update QuickSort.java

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,37 +1,57 @@
+package prac;
 
-class Solution
-{
-    //Function to sort an array using quick sort algorithm.
-    static void quickSort(int arr[], int low, int high)
-    {
-        // code here
-        if(low<high){
-            int pi = partition(arr,low,high);
-            quickSort(arr,low,pi-1);
-            quickSort(arr,pi+1,high);
-        }
-    }
-    static int partition(int arr[], int low, int high)
-    {
-        // your code here
-        int pivot = arr[high];
-        int i = low-1;
-        int j = low;
-        
-        for(;j<=high-1;j++){
-            if(arr[j]<pivot){
-                i++;
-                swap(arr,i,j);
-            }
-        }
-        swap(arr,i+1,j);
-        return i+1;
-    } 
-    
-    static void swap(int[] arr, int i, int j)
-{
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+import java.util.*;
+
+public class roughwork {
+	
+	
+	public static void main(String[] args) {
+		
+		int arr[]= {6,3,9,5,2,8,7};
+		
+		int l  = 0;
+		int r = arr.length-1;
+		
+		quicksort(arr,l,r);
+		
+		for(int i : arr) {
+			System.out.print(i+" ");
+		}
+		
+		
+	}
+	
+	public static void quicksort(int[] arr , int l , int r) {
+		
+		if(l<r) {
+			
+			int pivot = partition(arr,l,r);
+			
+			quicksort(arr,l,pivot-1);
+			quicksort(arr,pivot+1,r);
+		}
+	}
+	
+	public static int partition(int[] arr , int l , int r) {
+		int pivot  = arr[r];
+		int i = l-1;
+		int j = l;
+		
+		for(;j<=r-1;j++) {
+			if(arr[j]<pivot) {
+				i++;
+				swap(arr,i,j);
+			}
+		}
+		i++;
+		swap(arr,i,j);
+		return i;
+	}
+	
+	public static void swap(int arr[] , int i , int j) {
+		int temp = arr[i];
+		arr[i]=arr[j];
+		arr[j] = temp;
+	}
 }
-}
+
